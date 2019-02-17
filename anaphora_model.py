b24a44993d6cab16eef4041d4fd5b52bc201718a
Matthew Burke <mattburke122@gmail.com>
2019-02-17 15:29:11 -0600

    Added WikiCoref folder and started on predict_example

--- anaphora_model.py
+++ anaphora_model.py
@@ -1,7 +1,22 @@
 
+import allennlp
+
+
 class AnaphoraModel():
   """
   Writes out annotations for a document, represented as a list of tokens, in MMAX standoff format, to the path identified by the destination argument.
   """
+  model = allennlp.models.coreference_resolution.coref
+
+
+  ba_words = ./WikiCoref/Annotation/Barack_Obama/Barack Obama.txt
+
+  predictor = allennlp.predictors.coref
+
+  def dict_to_mmax(dict):
+    return dict
+
   def predict_example(words: list, destination: str):
-    pass
+    dict = predictor.predict_tokenized(words)
+    mmax = dict_to_mmax(dict)
+    return mmax
