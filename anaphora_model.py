12ef7c9c3c755b3c99076a6f7e77b5a50ad75102
Matthew Burke <mattburke122@gmail.com>
2019-02-17 14:51:23 -0600

    Added skeleton code from Kingland interface for model and trainer to be implemented.

---
+++ anaphora_model.py
@@ -0,0 +1,7 @@
+
+class AnaphoraModel():
+  """
+  Writes out annotations for a document, represented as a list of tokens, in MMAX standoff format, to the path identified by the destination argument.
+  """
+  def predict_example(words: list, destination: str):
+    pass
---
+++ anaphora_model_trainer.py
@@ -0,0 +1,7 @@
+
+class AnaphoraModelTrainer():
+  """
+  Trains an AnaphoraModel using data located in the provided list of paths, where a path is a string representing a path like WikiCoref/Annotation/Barack_Obama.
+  """
+  def train_model(paths: list):
+    pass
