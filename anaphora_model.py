f5efc1cdf03290c1c560cafbeb48a0441f93d7d0
Matthew Burke <mattburke122@gmail.com>
2019-02-27 17:39:31 -0600

    predict_example should work if e2e can be imported

--- anaphora_model.py
+++ anaphora_model.py
@@ -1,7 +1,6 @@
 
 import e2e-coref as e2e
-import neuralcoref as nc
-import
+import tensorflow as tf
 
 class AnaphoraModel():
   """
@@ -12,17 +11,24 @@ class AnaphoraModel():
 
 
 
-
-  def predict_example(words: list, destination: str):
-
-
-    file = open(destination, "a")
-    clusters = predictor.predict_tokenized(words)
-    file.write(clusters)
-
-
   def wordlist_to_block(words: list):
     block = ""
     for word in list:
       block += " " + word
     return block
+
+
+  def predict_example(words: list, destination: str):
+    text = wordlist_to_block(list)
+    config = e2e.util.initialize_from_env()
+    model = e2e.coref_model.CorefModel(config)
+    with tf.Session() as session:
+      model.restore(session)
+      e2e.demo.print_predictions(e2e.demo.make_predictions(text, model))
+      #e2e.demo.print_predictions_to_file(e2e.demo.make_predictions(text, model), destination)
+
+    # file = open(destination, "a")
+    # clusters = predictor.predict_tokenized(words)
+    # file.write(clusters)
+
+
