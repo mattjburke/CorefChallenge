a57592418fe61bf45536557df8dd81de4188cd54
Matthew Burke <mattburke122@gmail.com>
2019-03-01 20:40:31 -0600

    temporarily commented out glove curls to save time

--- e2eCoref/setup_all.sh
+++ e2eCoref/setup_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Download pretrained embeddings.
-curl -O http://lsz-gpu-01.cs.washington.edu/resources/glove_50_300_2.txt
-curl -O https://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip glove.840B.300d.zip
-rm glove.840B.300d.zip
+#curl -O http://lsz-gpu-01.cs.washington.edu/resources/glove_50_300_2.txt
+#curl -O https://nlp.stanford.edu/data/glove.840B.300d.zip
+#unzip glove.840B.300d.zip
+#rm glove.840B.300d.zip
 
 echo "about to build flags"
 # Build custom kernels.
